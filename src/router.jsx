1f673f9062ddccdb9c87b050610908228b7feb42
Lautaro Zuleta <lautarozule80@gmail.com>
2025-10-07 17:12:41 -0300

    add: router config

--- src/router.jsx
+++ src/router.jsx
@@ -1,6 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HeroView from "./views/home/HeroView";
 import HomeLayout from "./layouts/HomeLayout";
+import AuthLayout from "./layouts/AuthLayout";
+import NotFoundView from "./views/404/NotFoundView";
+import { ToastContainer } from "react-toastify";
+import LoginView from "./views/auth/LoginView";
+import RegisterView from "./views/auth/RegisterView";
 
 export default function router() {
     return (
@@ -9,7 +14,20 @@ export default function router() {
                 <Route path="/" element={<HomeLayout />}>
                     <Route index element={<HeroView />} />
                 </Route>
+
+                <Route path="/auth" element={<AuthLayout />}>
+                    <Route path="login" element={<LoginView />} index />
+                    <Route path="register" element={<RegisterView />} />
+                </Route>
+
+                <Route element={<AuthLayout />}>
+                    <Route path='*' element={<NotFoundView />} />
+                </Route>
             </Routes>
+            <ToastContainer
+                pauseOnHover={false}
+                pauseOnFocusLoss={false}
+            />
         </BrowserRouter>
     )
 }
