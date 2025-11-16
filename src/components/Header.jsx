07d64941f4611e6d5770d68bc0250b5ceb92d88d
Lautaro Zuleta <lautarozule80@gmail.com>
2025-11-16 20:46:37 -0300

    start with hero section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
 
     return (
         <header>
-            <div className="flex justify-between max-w-7xl mx-auto p-6">
+            <div className="flex items-center justify-between max-w-7xl mx-auto p-4 ">
                 <Link to="/">
                     🦈
                 </Link>
@@ -19,8 +19,8 @@ export default function Header() {
                     </div>
                 ) : (
                     <nav className="flex gap-4">
-                        <Link to="/auth/login">Iniciar Sesión</Link>
-                        <Link to="/auth/register">Registrarse</Link>
+                        <Link to="/auth/login" className='px-8 py-2 rounded-full hover:scale-105 transition duration-pro'>Iniciar Sesión</Link>
+                        <Link to="/auth/register" className='px-8 py-2 bg-gradient-to-r from-pink-500 to-orange-500 text-white rounded-full hover:scale-105 transition-transform duration-pro'>Registrarse</Link>
                     </nav>
                 )}
             </div>
--- src/views/home/HeroView.jsx
+++ src/views/home/HeroView.jsx
@@ -1,5 +1,31 @@
+import { Link } from "react-router";
+
 export default function HeroView() {
     return (
-        <div className="text-3xl font-extrabold text-center py-10">Bienvenido a Pescando 🦈</div>
+        <section className="relative py-30 px-4 sm:px-6 lg:px-8 bg-gradient-to-br from-purple-100 via-pink-50 to-orange-100 overflow-hidden">
+            <div className="flex gap-4 container mx-auto items-center justify-center relative">
+                <div className="absolute -top-10 -right-10 w-40 h-40 bg-orange-400 rounded-full opacity-50 blur-3xl"></div>
+                <div className="absolute -bottom-10 -left-10 w-60 h-60 bg-pink-400 rounded-full opacity-50 blur-3xl"></div>
+                <div className="w-1/2 flex flex-col gap-8 items-start">
+                    <h1 className="text-2xl md:text-4xl font-bold leading-tight">
+                        Deja de intentar.{' '}
+                        <span className="bg-gradient-to-r block from-pink-500 to-orange-500 bg-clip-text text-transparent">
+                            Empieza a pescar
+                        </span>{' '}
+                        hábitos que perduren.
+                    </h1>
+                    <p className="text-xl text-gray-700 text-balance">
+                        Con Pescando, diseña, rastrea y domina tus rutinas. Desde hábitos mensuales complejos
+                        hasta tareas diarias simples, todo en un solo lugar.
+                    </p>
+                    <Link to='/auth/register' className="z-10 px-8 py-4 bg-gradient-to-r from-pink-500 to-orange-500 text-white rounded-full text-lg font-semibold hover:shadow-2xl hover:scale-105 transition-all duration-pro">
+                        Comienza tu transformación
+                    </Link>
+                </div>
+                <div className="w-1/2 mask-radial-at-center mask-radial-from-60% mask-radial-to-80% mask-x-from-80% mask-x-to-93%">
+                    <video className="aspect-video" src="/pescando-habitos.mp4" autoPlay loop muted></video>
+                </div>
+            </div>
+        </section>
     )
 }
