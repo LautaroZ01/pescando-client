0dce134f7f6e227648b9ecfada46494287163447
Lautaro Zuleta <lautarozule80@gmail.com>
2025-11-19 11:25:58 -0300

    add: graph in feacture section

--- src/views/home/sections/FeactureView.jsx
+++ src/views/home/sections/FeactureView.jsx
@@ -21,17 +21,17 @@ export default function FeactureView() {
                         <p className="text-gray-500">No te limites. Crea hábitos semanales (como 'Ir al gimnasio 3 veces/sem') o mensuales (como 'Leer 1 libro al mes').</p>
                     </div>
                     <div className="relative overflow-hidden rounded-2xl z-10">
-                        <img src="/pex-con-idea.webp" alt="Pescando hábitos" className="rounded-2xl object-cover translate-x-4 ml-auto translate-y-24 max-w-96 group-hover:scale-105 transition-transform duration-400" />
+                        <img src="/pex-con-idea.webp" alt="Pescando hábitos" className="rounded-2xl object-cover aspect-square translate-x-4 ml-auto translate-y-24 max-w-96 group-hover:scale-105 transition-transform duration-400" />
                     </div>
                     <div className="absolute inset-0 m-auto w-60 h-60 bg-pink-400 rounded-full opacity-50 blur-3xl group-hover:bg-pink-500 transition-colors duration-300"></div>
                 </article>
-                <article className="row-span-2 flex items-start justify-between bg-gradient-to-br from-purple-100 via-pink-50 to-orange-100 rounded-2xl">
-                    <div>
-
-                    </div>
+                <article className="row-span-2 flex flex-col items-start justify-between bg-gradient-to-br from-purple-100 via-pink-50 to-orange-100 rounded-2xl group">
                     <div className="space-y-4 p-6">
                         <h3 className="text-2xl font-bold">Tu <span className="text-style">progreso</span>, claro y visual</h3>
                         <p className='text-gray-600 leading-relaxed'>Desde tu dashboard personal podrás ver qué toca hoy. Completa tus hábitos y edita tus rutinas..</p>
+                    </div>
+                    <div className="relative rounded-2xl z-10  w-full max-h-30">
+                        <img src="/grafica.webp" alt="Pescando hábitos" className="rounded-2xl aspect-square -translate-y-10 object-cover ml-auto max-w-40 group-hover:scale-105 transition-transform duration-400" />
                     </div>
                 </article>
                 <article className="row-span-2 col-start-2 row-start-3 bg-gradient-to-br from-purple-100 via-pink-50 to-orange-100 rounded-2xl p-6 flex flex-col justify-start relative overflow-hidden group">
@@ -46,7 +46,7 @@ export default function FeactureView() {
                             Únete gratis para sincronizar tus hábitos en la nube y acceder desde cualquier dispositivo.
                         </p>
 
-                        <Link className="btn-primary px-6 py-2.5 rounded-full text-sm font-medium hover:shadow-lg  flex items-center gap-2">
+                        <Link className="btn-primary px-6 py-2.5 rounded-full text-sm font-medium hover:shadow-lg  flex items-center gap-2 mt-12">
                             Crear cuenta gratis
                             <FaArrowRight />
                         </Link>
