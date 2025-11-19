baf06c914c783cb6bcfb7f9d7b26fc771f090117
Lautaro Zuleta <lautarozule80@gmail.com>
2025-11-19 11:25:23 -0300

    ending community section

--- src/views/home/sections/CommunityView.jsx
+++ src/views/home/sections/CommunityView.jsx
@@ -1,6 +1,9 @@
+import { LuCopy } from "react-icons/lu";
+import { FaUsers } from "react-icons/fa";
+
 export default function CommunityView() {
     return (
-        <section id="comunidad" className="py-20 px-4 sm:px-6 lg:px-8 bg-gradient-to-br from-pink-50 to-orange-50">
+        <section id="comunidad" className="py-40 px-4 sm:px-6 lg:px-8 bg-gradient-to-br from-pink-50 to-orange-50">
             <div className="text-center mb-16">
                 <h2 className="text-4xl font-bold mb-4">
                     No lo hagas solo.{' '}
@@ -9,6 +12,30 @@ export default function CommunityView() {
                     </span>
                 </h2>
                 <p className="text-xl text-gray-700">La motivación es más fácil cuando se comparte.</p>
+            </div>
+            <div className="grid gap-6 grid-cols-3 container mx-auto">
+                <article className="bg-white rounded-2xl p-10 space-y-8">
+                    <img src="/medalla-de-oro.webp" alt="Medalla pescando" className="size-20 object-contain aspect-square" />
+                    <div className="space-y-2">
+                        <h4 className="text-lg font-bold">Comparte Logros</h4>
+                        <p>Publica tus victorias, comparte tus rachas y recibe apoyo de otros usuarios que están en el mismo camino.</p>
+
+                    </div>
+                </article>
+                <article className="bg-white rounded-2xl p-10 space-y-8">
+                    <LuCopy className="size-20 text-pink-500" />
+                    <div className="space-y-2">
+                        <h4 className="text-lg font-bold">Copia Hábitos</h4>
+                        <p>¿Viste un hábito interesante que creó otro usuario? Cópialo a tu propio dashboard con un solo clic y empieza a construirlo.</p>
+                    </div>
+                </article>
+                <article className="bg-white rounded-2xl p-10 space-y-8">
+                    <FaUsers className="size-20 text-orange-500" />
+                    <div className="space-y-2">
+                        <h4 className="text-lg font-bold">+10,000</h4>
+                        <p>Usuarios transformando sus vidas. No necesitas empezar de cero. Inspírate explorando los hábitos de nuestra comunidad.</p>
+                    </div>
+                </article>
             </div>
         </section>
     )
